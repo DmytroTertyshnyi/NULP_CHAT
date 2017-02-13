83e69f1d417fdee4dbc5a4a52289b7e7169bdcae
DmytroTertyshnyi <Dmytro.Tertyshnyi.MTR.2016@lpnu.ua>
2017-02-13 11:32:53 +0200

    TCP client commit

---
+++ TcpSocektClient/src/main/java/TcpSocektClient/TcpSocektClient/App.java
@@ -0,0 +1,13 @@
+package TcpSocektClient.TcpSocektClient;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
